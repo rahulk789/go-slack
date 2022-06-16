35ba4e418cbf892b63d5f3600868a461d35adb58
rahulk789 <rahul.u.india@gmail.com>
2022-06-16 17:34:26 +0530

    added scoket io

---
+++ socket.go
@@ -0,0 +1,110 @@
+package main
+ 
+import (
+    "context"
+    "fmt"
+    "log"
+    "os"
+    "time"
+ 
+    "github.com/joho/godotenv"
+    "github.com/slack-go/slack"
+    "github.com/slack-go/slack/slackevents"
+    "github.com/slack-go/slack/socketmode"
+)
+ 
+func main() {
+ 
+    godotenv.Load(".env")
+ 
+    token := os.Getenv("SLACK_AUTH_TOKEN")
+    appToken := os.Getenv("SLACK_APP_TOKEN")
+ 
+    client := slack.New(token, slack.OptionDebug(true), slack.OptionAppLevelToken(appToken))
+ 
+    socketClient := socketmode.New(
+        client,
+        socketmode.OptionDebug(true),
+        socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+    )
+ 
+    ctx, cancel := context.WithCancel(context.Background())
+ 
+    defer cancel()
+ 
+    go func(ctx context.Context, client *slack.Client, socketClient *socketmode.Client) {
+        for {
+            select {
+            case <-ctx.Done():
+                log.Println("Shutting down socketmode listener")
+                return
+            case event := <-socketClient.Events:
+ 
+                switch event.Type {
+        
+                case socketmode.EventTypeEventsAPI:
+ 
+                    eventsAPI, ok := event.Data.(slackevents.EventsAPIEvent)
+                    if !ok {
+                        log.Printf("Could not type cast the event to the EventsAPI: %v\n", event)
+                        continue
+                    }
+ 
+                    socketClient.Ack(*event.Request)
+                err := HandleEventMessage(eventsAPI, client)
+                if err != nil {
+                    log.Fatal(err)
+                    }
+                }
+            }
+        }
+    }(ctx, client, socketClient)
+ 
+    socketClient.Run()
+}
+func HandleEventMessage(event slackevents.EventsAPIEvent, client *slack.Client) error {
+    switch event.Type {
+ 
+    case slackevents.CallbackEvent:
+ 
+        innerEvent := event.InnerEvent
+ 
+        switch evnt := innerEvent.Data.(type) {
+            err := HandleAppMentionEventToBot(evnt, client)
+            if err != nil {
+                return err
+            }
+        }
+    default:
+        return errors.New("unsupported event type")
+    }
+    return nil
+}
+func HandleAppMentionEventToBot(event *slackevents.AppMentionEvent, client *slack.Client) error {
+ 
+    user, err := client.GetUserInfo(event.User)
+    if err != nil {
+        return err
+    }
+ 
+    text := strings.ToLower(event.Text)
+ 
+    attachment := slack.Attachment{}
+ 
+    if strings.Contains(text, "hello") || strings.Contains(text, "hi") {
+        attachment.Text = fmt.Sprintf("Hello %s", user.Name)
+        attachment.Color = "#4af030"
+    } else if strings.Contains(text, "weather") {
+        attachment.Text = fmt.Sprintf("Weather is sunny today. %s", user.Name)
+        attachment.Color = "#4af030"
+    } else {
+        attachment.Text = fmt.Sprintf("I am good. How are you %s?", user.Name)
+        attachment.Color = "#4af030"
+    }
+    _, _, err = client.PostMessage(event.Channel, slack.MsgOptionAttachments(attachment))
+    if err != nil {
+        return fmt.Errorf("failed to post message: %w", err)
+    }
+    return nil
+}
+
